664c5fcf516f32cbdb28b3d5abdc5c1d31b734de
emanuelcastanaza18 <ecastanazac@miumg.edu.gt>
2023-04-19 17:36:01 -0600

    Se termino de estudiar la seccion 8

--- tipos-avanzados/08-comprension-listars.py
+++ tipos-avanzados/08-comprension-listars.py
@@ -13,13 +13,35 @@
 #     nombres.append(usuario[0])
 # print(nombres)
 
-#Otra forma de hacerlo
+# Para modificar los elementos de una lista
+# usuarios = [
+#     ["chanchito", 4],
+#     ["Felipe", 1],
+#     ["Pulga", 5]
+# ]
+
+# # nombres = [expresion for item in items]
+# nombres = [usuario[0] for usuario in usuarios]
+# print(nombres)
+
+# Para filtrarlo
+# usuarios = [
+#     ["chanchito", 4],
+#     ["Felipe", 1],
+#     ["Pulga", 5]
+# ]
+
+# # nombres = [expresion for item in items if lista [id] ]
+# nombres = [usuario for usuario in usuarios if usuario[1] > 2]
+# print(nombres)
+
+#Modificar y filtarla
 usuarios = [
     ["chanchito", 4],
     ["Felipe", 1],
     ["Pulga", 5]
 ]
 
-# nombres = [expresion for item in items]
-nombres = [usuario[0] for usuario in usuarios]
+# nombres = [expresion[indice] for item in items if lista [id] ]
+nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
 print(nombres)
